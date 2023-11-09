4cecf3037624fb21bd5afe64c1b9d721e19e6893
Gleb Shishov <glebshishov@gmail.com>
2023-11-09 14:49:54 +0600

    исправил зум на мобилках , поправил футер, изменил трейлер

--- js/Gallery.js
+++ js/Gallery.js
@@ -1,22 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   var modal = document.getElementById("imageModal");
   var modalImg = document.getElementById("modalImage");
   var thumbnails = document.getElementsByClassName("thumbnail");
   var closeBtn = document.getElementsByClassName("close")[0];
 
+  function isMobile() {
+    return window.innerWidth < 768;
+  }
+
   for (var i = 0; i < thumbnails.length; i++) {
-    thumbnails[i].addEventListener("click", function () {
-      modal.style.display = "flex";
-      modalImg.src = this.src;
+    thumbnails[i].addEventListener("click", function() {
+      if (!isMobile()) {
+        modal.style.display = "flex";
+        modalImg.src = this.src;
+      }
     });
   }
 
-  closeBtn.addEventListener("click", function () {
+  closeBtn.addEventListener("click", function() {
     modal.style.display = "none";
   });
 
-  modal.addEventListener("click", function (e) {
-    if (e.target === modal) {
+  modal.addEventListener("click", function(e) {
+    if (!isMobile() && e.target === modal) {
       modal.style.display = "none";
     }
   });
