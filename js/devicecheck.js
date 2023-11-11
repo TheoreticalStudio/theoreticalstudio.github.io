cd54cf156233cae9d154bec895641037b8a23567
Gleb Shishov <glebshishov@gmail.com>
2023-11-12 03:24:59 +0600

    тест 11 ( или возврат)

--- js/devicecheck.js
+++ js/devicecheck.js
@@ -17,31 +17,8 @@
 //     GooglePlay.classList.add("store-button");
 //   }
 // });
-// document.addEventListener("DOMContentLoaded", function() {
-//   var AppStore = document.getElementById("AppStore");
-//   var GooglePlay = document.getElementById("GooglePlay");
-//
-//   var platform = navigator.platform.toLowerCase();
-//
-//   // Проверка, является ли устройство устройством Apple
-//   var isAppleDevice = /iphone|ipad|ipod|mac/.test(platform);
-//
-//   // Проверка, является ли устройство устройством Android
-//   var isAndroidDevice = /android/.test(platform);
-//
-//   // Проверка, является ли устройство компьютером
-//   var isDesktop = !isAppleDevice && !isAndroidDevice;
-//
-//   // В зависимости от типа устройства активируем соответствующие кнопки
-//   if (isAppleDevice) {
-//     AppStore.classList.add("on");
-//   } else if (isAndroidDevice) {
-//     GooglePlay.classList.add("on");
-//   } else if (isDesktop) {
-//     AppStore.classList.add("on");
-//     GooglePlay.classList.add("on");
-//   }
-// });
+
+
 document.addEventListener("DOMContentLoaded", function() {
   var appStore = document.getElementById("appStore");
   var googlePlay = document.getElementById("googlePlay");
@@ -61,3 +38,4 @@ document.addEventListener("DOMContentLoaded", function() {
     googlePlay.classList.add("on");
   }
 });
+
