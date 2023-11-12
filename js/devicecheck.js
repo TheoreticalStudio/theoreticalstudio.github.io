f9422db465f2321713f097e71f1d81f4c892e937
Gleb Shishov <glebshishov@gmail.com>
2023-11-12 12:26:08 +0600

    исправил показ кнопок сторов

--- js/devicecheck.js
+++ js/devicecheck.js
@@ -25,13 +25,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var platform = navigator.platform.toLowerCase();
 
-  var isAppleDevice = /iphone|ipad|ipod|mac/.test(platform);
+  var isAppleDevice = /iphone|ipad|ipod/.test(platform);
   var isAndroidDevice = /android/.test(platform);
   var isDesktop = !isAppleDevice && !isAndroidDevice;
 
+
   if (isAppleDevice) {
     appStore.classList.add("on");
+    googlePlay.classList.add("off");
   } else if (isAndroidDevice) {
+    appStore.classList.add("off");
     googlePlay.classList.add("on");
   } else if (isDesktop) {
     appStore.classList.add("on");
